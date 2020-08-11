aca29a0509c2dae673fab66118cf600cf00568a2
dantelacu2 <55060638+dantelacu2@users.noreply.github.com>
2020-08-10 21:48:46 -0400

    Updates

---
+++ precache-manifest.7192edf143d108f9b97e8d930e749cb8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac5ba948529ee3c663086ee1ab76cc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbd1bdf22acbba436856",
+    "url": "/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "72ce37a4f1d4bb8a3438",
+    "url": "/static/css/main.21f2dad7.chunk.css"
+  },
+  {
+    "revision": "bbd1bdf22acbba436856",
+    "url": "/static/js/2.57318be5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.57318be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ce37a4f1d4bb8a3438",
+    "url": "/static/js/main.4c14e86c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9c7e1f10dd91563350b4c627032282bb",
+    "url": "/static/media/MongoLogo.9c7e1f10.png"
+  },
+  {
+    "revision": "cc791bec145b9d9a77a047629fe32fd7",
+    "url": "/static/media/PythonLogo.cc791bec.png"
+  },
+  {
+    "revision": "ee722475bcaaf759acce99ce7b9dc9a8",
+    "url": "/static/media/ReactLogo.ee722475.png"
+  },
+  {
+    "revision": "eee0b154e0326329b011aea3120c7835",
+    "url": "/static/media/bird-cells-new.eee0b154.svg"
+  },
+  {
+    "revision": "7df735480bb9777c817f3afb449ea324",
+    "url": "/static/media/me_avatar.7df73548.png"
+  },
+  {
+    "revision": "5958582743119da78f1fd3726dd8edfa",
+    "url": "/static/media/me_working.59585827.png"
+  }
+]);
