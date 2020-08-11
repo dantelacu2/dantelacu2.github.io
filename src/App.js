6a955df1c5f0d3dfe65c664837b6bd1909f919ba
dantelacu2 <55060638+dantelacu2@users.noreply.github.com>
2020-08-11 18:56:25 -0400

    remove browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Switch, Route } from 'react-router-dom';
 
 // reused components
