590edaecfc277889db9db21447c76686907b2431
dantelacu2 <55060638+dantelacu2@users.noreply.github.com>
2020-08-11 23:43:58 -0400

    change

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -62,6 +62,7 @@ class Contact extends React.Component {
             // }, (error) => {
             //     console.log(error.text);
             // });
+            e.preventDefault();
             fetch("/success", {
                 method: "POST",
                 headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -69,7 +70,6 @@ class Contact extends React.Component {
             })
             .then(() => console.log("sent"))
             .catch(error => console.log(error));
-            e.preventDefault();
 
             this.state.myMessages
             .push(<Message 
@@ -135,11 +135,11 @@ class Contact extends React.Component {
                 {this.state.messageSent ? <Message content="Thank you! Dante will get back to you soon."/> : <></>}
                 <div className={formClass}>
                     <hr className={classes.hr} />
-                    <form action="/success" method="POST" name="myForm" id="myForm" onSubmit={this.handleSubmit} netlify data-netlify="true">
+                    <Form name="myForm" id="myForm" onSubmit={this.handleSubmit} netlify data-netlify="true">
                         <Form.Group>
                             <Form.Control className={classes.input} disabled={this.state.isDisabled} type="text" name="message" id="message" value={this.state.messageBody} onChange={this.handleChange} placeholder="Enter your message to Dante" />
                         </Form.Group>
-                    </form>
+                    </Form>
                 </div>
             </Container>
         );
